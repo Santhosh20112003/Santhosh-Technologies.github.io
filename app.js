80280d8b69458ca62a07347486fb8dce419ee72f
Santhosh <shanmugamsanthosh22@gmail.com>
2023-03-03 15:32:32 +0530

    cnhdb

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
+		header.style.backgroundColor = '#203354';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
